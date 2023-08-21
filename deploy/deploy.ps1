0742353a8914f2be6c13d1a00256acd9727c6ef4
Matthew Garrett <mgarrett0402@gmail.com>
2023-08-21 10:40:29 -0700

    Added output and adjusted workflow steps to properly remove App Registrations by ID instead of Name

--- deploy/deploy.ps1
+++ deploy/deploy.ps1
@@ -1028,6 +1028,8 @@ process {
     Write-Host
     Stop-Transcript | Out-Null
 
+    Write-Output "ipamUIAppId=$($appDetails.UIAppId)" >> $Env:GITHUB_OUTPUT
+    Write-Output "ipamEngineAppId=$($appDetails.EngineAppId)" >> $Env:GITHUB_OUTPUT
     Write-Output "ipamSuffix=$($deployment.Outputs["suffix"].Value)" >> $Env:GITHUB_OUTPUT
     Write-Output "ipamResourceGroup=$($deployment.Outputs["resourceGroupName"].Value)" >> $Env:GITHUB_OUTPUT
 
