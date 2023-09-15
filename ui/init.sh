0118bb755ed852e2a6ce542e7341279545d6a379
Matthew Garrett <mgarrett0402@gmail.com>
2023-09-14 20:59:06 -0700

    Replaced http-server with Vercel serve

--- ui/init.sh
+++ ui/init.sh
@@ -30,4 +30,5 @@ echo "}" >> env.js
 mv env.js ./dist/
 
 # npx --yes react-inject-env set
-npx --yes http-server -a 0.0.0.0 -P http://localhost:${PORT}? -p ${PORT} dist
+# npx --yes http-server -a 0.0.0.0 -P http://localhost:${PORT}? -p ${PORT} dist
+npx serve -s -L -p ${PORT} dist
