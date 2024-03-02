887ca042c032cbeebab9da07ec8e9ba04ee906eb
Matthew Garrett <mgarrett0402@gmail.com>
2024-03-02 14:40:38 -0800

    Updated init script to conditionally modify environment variables

--- init.sh
+++ init.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 PORT=$1
 
+if [ $WEBSITE_RUN_FROM_PACKAGE = "1" ]; then
+  export PATH=$PATH:$APP_PATH/packages
+  export PYTHONPATH=$PYTHONPATH:$APP_PATH/packages
+fi
+
 # Pull Environment Variables from Parent Shell
 eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
 
