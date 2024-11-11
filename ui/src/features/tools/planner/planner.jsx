11e8493a2dac8154b87526df9b097aa00f663c22
Matthew Garrett <mgarrett0402@gmail.com>
2024-11-10 21:45:31 -0800

    Fixed Grid2 import as a result of upgrading from MUI v5 to v6

--- ui/src/features/tools/planner/planner.jsx
+++ ui/src/features/tools/planner/planner.jsx
@@ -20,7 +20,7 @@ import {
   CircularProgress
 } from "@mui/material";
 
-import Grid from "@mui/material/Unstable_Grid2";
+import Grid from "@mui/material/Grid2";
 
 import {
   FilterList as FilterListIcon,
