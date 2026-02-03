0257ba30bae783ac7168f6a626a2e7c6da9f7ffe
Matthew Garrett <mgarrett0402@gmail.com>
2026-02-03 12:43:50 -0800

    Aligned MSAL initialization with current best practices

--- ui/src/index.jsx
+++ ui/src/index.jsx
@@ -12,14 +12,24 @@ import { msalConfig } from "./msal/authConfig";
 const container = document.getElementById('root');
 const root = createRoot(container);
 
+/**
+ * MSAL should be instantiated outside of the component tree to prevent it from being re-instantiated on re-renders.
+ * For more, visit: https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-react/docs/getting-started.md
+ */
 export const msalInstance = new PublicClientApplication(msalConfig);
 
-root.render(
-  <React.StrictMode>
-    <MsalProvider instance={msalInstance}>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </MsalProvider>
-  </React.StrictMode>
-);
+/**
+ * Initialize MSAL before rendering the app.
+ * This is required for msal-browser v3+ to properly set up the library.
+ */
+msalInstance.initialize().then(() => {
+  root.render(
+    <React.StrictMode>
+      <MsalProvider instance={msalInstance}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </MsalProvider>
+    </React.StrictMode>
+  );
+});
