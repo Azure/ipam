27a92873328f12ef185a3e3f7640d77fbfe460ff
Matthew Garrett <mgarrett0402@gmail.com>
2026-03-12 22:02:44 -0700

    Upgraded to Vite v8, plugin-react v6 and removed vite-plugin-eslint2

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import eslint from "vite-plugin-eslint2";
 
 export default () => {
   return defineConfig({
@@ -9,13 +8,6 @@ export default () => {
     // },
     plugins: [
       react(),
-      eslint({
-        cache: false,
-        lintOnStart: true,
-        lintInWorker: true,
-        include: ["src/**/*.js", "src/**/*.jsx", "src/**/*.ts", "src/**/*.tsx"],
-        exclude: [],
-      }),
       {
         name: "build-ui-html",
         apply: "build",
